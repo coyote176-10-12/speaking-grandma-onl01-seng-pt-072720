2760d792807f413f6e0ef20eec945fd0df0571df
Javier Aguilar <coyote176@msn.com>
2020-07-30 03:08:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,4 +9,17 @@ if phrase == "I LOVE YOU GRANDMA!"
   		return "NO, NOT SINCE 1938!"
     end
     
-    
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    # If you shout, she can hear you (or at least she thinks so)
+    # and yells back
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    # NO, NOT SINCE 1938!
+    else
+  		return "NO, NOT SINCE 1938!"
+    end
