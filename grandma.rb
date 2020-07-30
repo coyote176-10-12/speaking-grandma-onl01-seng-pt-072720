0662814d4760fa84f62bba96727f02905046f018
Javier Aguilar <coyote176@msn.com>
2020-07-30 04:10:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,4 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     phrase = "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase == phrase
-    phrase = "NO, NOT SINCE 1938!"
-  else phrase = 'HUH?! SPEAK UP, SONNY!'
-  end
-  phrase
 end
