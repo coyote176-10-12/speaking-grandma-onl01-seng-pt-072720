2196f803f47c2a32c8ad3d340a8d4b10f3a560a9
Javier Aguilar <coyote176@msn.com>
2020-07-30 03:50:15 +0000

    Automatically backed up by Learn

---
+++ grandma.rb
@@ -0,0 +1,9 @@
+def speak_to_grandma(words)
+  if words == "I LOVE YOU GRANDMA!"
+    words = "I LOVE YOU TOO PUMPKIN!"
+  elsif words.upcase == words
+    words = "NO, NOT SINCE 1938!"
+  else words = 'HUH?! SPEAK UP, SONNY!'
+  end
+  words
+end
