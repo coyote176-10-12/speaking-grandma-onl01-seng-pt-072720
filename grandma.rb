3f83ee18e8ab3273cb30b8854bb4695e5eed6fa0
Javier Aguilar <coyote176@msn.com>
2020-07-30 02:37:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma (phase)
-if 
+if (phrase == "I LOVE YOU GRANDMA!")
+  return "I LOVE YOU TOO PUMPKIN!"
+
 
 # Write a speak_to_grandma method.
 
@@ -14,3 +16,12 @@ if
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(phrase)
+  if(phrase=="I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase=="#{phrase}".upcase)
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
