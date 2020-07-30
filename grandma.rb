87c91dccf3ee3ae2a9a7a639771fc1b5febc7a7c
Javier Aguilar <coyote176@msn.com>
2020-07-30 03:13:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,27 +1,8 @@
-def speak_to_grandma(phrase)
-if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-     return "HUH?! SPEAK UP, SONNY!"
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
-    
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    # If you shout, she can hear you (or at least she thinks so)
-    # and yells back
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
-    
-    
+ def speak_to_grandma(phrase)
+   if phrase == "I LOVE YOU GRANDMA!"
+	   "I LOVE YOU TOO PUMPKIN"
+	 elsif phrase != phrase.upcase
+			"HUH!? SPEAK UP SONNY!"
+	 else phrase == phrase.upcase 
+			"NO, NOT SINCE 1938!"     
+	end
